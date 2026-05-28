69ecfc80d04981cbc1e8b9613e7dc0c8daa34506
ankitlodhi11 <Ankit@balbhadrat81gmail.onmicrosoft.com>
2026-05-28 11:33:49 +0530

    uma

--- provider.tf
+++ provider.tf
@@ -5,7 +5,16 @@ terraform {
       version = "4.57.0"
     }
   }
+ 
+  backend "azurerm" {
+    resource_group_name = "newrgankit"
+    storage_account_name = "storqagedataankit"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
+
+
 
 provider "azurerm" {
   features {}
